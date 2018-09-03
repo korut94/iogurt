5cfd6cdcf0be76d791479904cae16ae3f4842357
Andrea Mantovani <andreamnt94@gmail.com>
2018-09-03 10:29:41 +0100

    Created draft for the main menu UI with relative scripts

---
+++ Assets/Tools/Main/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.EditorVR;
+using UnityEditor.Experimental.EditorVR.Menus;
+using UnityEngine;
+
+namespace Iogurt.Tools
+{
+    public class MainMenu : MonoBehaviour, IMenu
+    {
+        public Bounds       localBounds { get; private set; }
+        public GameObject   menuContent { get { return gameObject; } }
+
+        public MenuHideFlags menuHideFlags {
+            get { return gameObject.activeSelf ? 0 : MenuHideFlags.Hidden; }
+            set { gameObject.SetActive(value == 0); }
+        }
+        
+        public int priority { get { return 1; } }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Tools/Main/MainTool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.EditorVR;
+using UnityEngine;
+
+namespace Iogurt.Tools
+{
+    [MainMenuItem("Iogurt", "Extensions", "Starting Iogurt Editor Extension")]
+    sealed class MainTool : MonoBehaviour, ITool, IConnectInterfaces, IInstantiateMenuUI,
+        IUsesRayOrigin
+    {
+        [SerializeField]
+        MainMenu m_menuPrefab;
+
+        public Transform rayOrigin { get; set; }
+
+        GameObject m_toolMenu;
+
+        void Start() {
+            // Clear selection so we can't manipulate things
+            UnityEditor.Selection.activeGameObject = null;
+
+            m_toolMenu = this.InstantiateMenuUI(rayOrigin, m_menuPrefab);
+            var mainMenu = m_toolMenu.GetComponent<MainMenu>();
+
+            this.ConnectInterfaces(mainMenu, rayOrigin);
+        }
+    }
+}
