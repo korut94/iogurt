faf772ae6945530c297f2b6284d3843c67664474
Andrea Mantovani <andreamnt94@gmail.com>
2018-10-01 08:44:55 +0200

    Added interfaces for Touch, ProceduralAnimation and UI

---
+++ Assets/Scripts/Interfaces/Input/Touch/IUsesSwipeGesture.cs
@@ -0,0 +1,17 @@
+﻿using DigitalRubyShared;
+
+namespace Iogurt.Input.Touch
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IUsesSwipeGesture
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="direction"></param>
+        void Swipe(SwipeGestureRecognizerDirection direction, float speed);
+    }
+}
+
