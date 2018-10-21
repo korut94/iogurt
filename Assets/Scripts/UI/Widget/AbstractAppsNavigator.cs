d62b222244e7072c1d530799a07a014d915e64eb
Andrea Mantovani <andreamnt94@gmail.com>
2018-10-21 16:19:02 +0200

    Changed navigation focus over the applications instead of the menu

---
+++ Assets/Scripts/UI/Widget/AbstractAppsNavigator.cs
@@ -0,0 +1,14 @@
+﻿using Iogurt.UI.Applications;
+using RSG;
+using UnityEngine;
+
+namespace Iogurt.UI
+{
+    public abstract class AbstractAppsNavigator : AbstractScrollView, IAppsNavigator
+    {
+        public abstract IPromise Next();
+        public abstract IPromise Previous();
+        public abstract IPromise SpawnApplication(GameObject app);
+    }
+}
+
