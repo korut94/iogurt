d77856ae592c3ee12828cf96b005f4b3bccb3b7d
Andrea Mantovani <andreamnt94@gmail.com>
2018-11-10 17:06:08 +0100

    Defined script for application Link and List

---
+++ Assets/Scripts/UI/Widget/ApplicationLink.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Iogurt.UI
+{
+    public class ApplicationLink : MonoBehaviour
+    {
+        public Sprite icon
+        {
+            set
+            {
+                var iconTransform = transform.Find("Icon");
+
+                if (iconTransform == null)
+                    Debug.LogError("No Icon element was found in the current object");
+                else
+                    iconTransform.GetComponent<Image>().sprite = value;
+            }
+        }
+
+        public string title
+        {
+            set
+            {
+                var textTransform = transform.Find("Title");
+
+                if (textTransform == null)
+                    Debug.LogError("No Text element was found in the current object");
+                else
+                    textTransform.gameObject.GetComponent<TextMeshProUGUI>().SetText(value);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Widget/ApplicationsList.cs
@@ -0,0 +1,35 @@
+﻿using Iogurt.Applications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Iogurt.UI
+{
+    public sealed class ApplicationsList : AbstractWidget, IUsesListOfApplications
+    {
+        [SerializeField]
+        ApplicationLink ApplicationLinkPrefab;
+
+        public IEnumerable<Type> applications { get; set; }
+
+        void Start()
+        {
+            foreach (var application in applications)
+            {
+                var applicationInfo = application.GetCustomAttributes(typeof(ApplicationItem), false);
+                if (applicationInfo.Length == 0)
+                    Debug.LogWarning("The " + application.Name + " does not provide any relative informations about its application. Skipped.");
+                else
+                {
+                    var item = applicationInfo.First() as ApplicationItem;
+                    var go = Instantiate(ApplicationLinkPrefab, transform);
+                    var link = go.GetComponent<ApplicationLink>();
+
+                    link.title = item.title;
+                }
+            }
+        }
+    }
+}
+
