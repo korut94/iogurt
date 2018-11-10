1fe5bb9d598838eb23fcc117ec82de2fbf0eb7f0
Andrea Mantovani <andreamnt94@gmail.com>
2018-11-10 17:03:55 +0100

    Renamed in IUseLoadedApplications

---
+++ Assets/Scripts/Interfaces/FunctionalityInjection/IUsesLoadedApplications.cs
@@ -0,0 +1,11 @@
+﻿using Iogurt.UI.Applications;
+using System.Collections.Generic;
+
+namespace Iogurt
+{
+    public interface IUsesLoadedApplications
+    {
+        IList<IApplication> LoadedApplications { set; }
+    }
+}
+
