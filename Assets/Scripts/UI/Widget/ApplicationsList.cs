d66464c9bd3da48a0952f9c279dbc7450030f91d
Andrea Mantovani <andreamnt94@gmail.com>
2018-11-22 15:11:47 +0100

    ApplicationList retrieve the app's icon from the prefabs

--- Assets/Scripts/UI/Widget/ApplicationsList.cs
+++ Assets/Scripts/UI/Widget/ApplicationsList.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace Iogurt.UI
 {
-    public sealed class ApplicationsList : AbstractWidget, IUsesListOfApplications
+    public sealed class ApplicationsList : AbstractWidget, IUsesListOfApplications, ILoadTool, IUsesApplicationIcon
     {
         [SerializeField]
         ApplicationLink ApplicationLinkPrefab;
@@ -15,18 +15,22 @@ namespace Iogurt.UI
 
         void Start()
         {
-            foreach (var application in applications)
+            foreach (var tool in applications)
             {
-                var applicationInfo = application.GetCustomAttributes(typeof(ApplicationItem), false);
+                var applicationInfo = tool.GetCustomAttributes(typeof(AppItem), false);
                 if (applicationInfo.Length == 0)
-                    Debug.LogWarning("The " + application.Name + " does not provide any relative informations about its application. Skipped.");
-                else
                 {
-                    var item = applicationInfo.First() as ApplicationItem;
+                    Debug.LogWarning("The " + tool.Name + " does not provide any relative informations about its application. Skipped.");
+                } 
+                else 
+                {
+                    var item = applicationInfo.First() as AppItem;
                     var go = Instantiate(ApplicationLinkPrefab, transform);
                     var link = go.GetComponent<ApplicationLink>();
 
                     link.title = item.title;
+                    link.icon = this.GetApplicationIcon(tool);
+                    link.onClick.AddListener(() => { this.LoadTool(tool); });
                 }
             }
         }
