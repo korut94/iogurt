ba8902d492227ab8108b60afe6f351f155e435b7
Andrea Mantovani <andreamnt94@gmail.com>
2018-11-22 15:36:38 +0100

    Replaced CreatePrimitiveObject with a more general application

---
+++ Assets/Tools/SpawnObjectsTool/SpawnObjectsTool.cs
@@ -0,0 +1,25 @@
+﻿using Iogurt.Applications;
+using UnityEngine;
+
+namespace Iogurt.Tools
+{
+    [AppItem("Create Primitive")]
+    public class SpawnObjectsTool : MonoBehaviour, ITool, IConnectInterface, IInstantiateApplicationUI, IApplicationIcon
+    {
+        [SerializeField]
+        SpawnObjectsApp ApplicationPrefab;
+        [SerializeField]
+        Sprite Icon;
+
+        GameObject m_application;
+
+        public UI.Application application { get { return m_application.GetComponent<SpawnObjectsApp>(); } }
+        public Sprite icon { get { return Icon; } }
+
+        void Start()
+        {
+            m_application = this.InstantiateApplicationUI(ApplicationPrefab);
+            this.ConnectInterfaces(m_application);
+	    }
+    }
+}
