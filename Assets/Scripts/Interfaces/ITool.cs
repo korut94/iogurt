3b8218747b6bff98678b8393dbe23f979054edf5
Andrea Mantovani <andreamnt94@gmail.com>
2018-11-22 15:01:50 +0100

    Function cheker the right signature of a tool

--- Assets/Scripts/Interfaces/ITool.cs
+++ Assets/Scripts/Interfaces/ITool.cs
@@ -1,10 +1,15 @@
-﻿using Iogurt.UI;
+﻿using UnityEngine;
 
 namespace Iogurt
 {
     public interface ITool
     {
-        Application application { get; }
+        UI.Application application { get; }
+    }
+
+    public static class IToolMethods
+    {
+        public static bool HasRightSignature(this ITool obj) { return obj is MonoBehaviour; }
     }
 }
 
