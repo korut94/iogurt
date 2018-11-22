fa9237b6436dcf197691d7f74a6f7a5a1b6ba845
Andrea Mantovani <andreamnt94@gmail.com>
2018-11-22 15:35:59 +0100

    Added AppItem attribute for Tool

---
+++ Assets/Scripts/UI/Applications/AppItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Iogurt.Applications
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AppItem : Attribute
+    {
+        string m_description;
+        string m_title;
+
+        public string description { get { return m_description; } }
+        public string title { get { return m_title; } }
+
+        public AppItem(string title, string description = null)
+        {
+            m_title = title;
+            m_description = description;
+        }
+    }
+}
+
