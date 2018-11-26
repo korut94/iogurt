4cc7ec7adc920856e33136b73eacff061e3f9874
Andrea Mantovani <andreamnt94@gmail.com>
2018-11-26 16:28:10 +0100

    ScrollRect content as AppsNavigator content

--- Assets/Scripts/UI/Widget/AbstractAppsNavigator.cs
+++ Assets/Scripts/UI/Widget/AbstractAppsNavigator.cs
@@ -6,6 +6,8 @@ namespace Iogurt.UI
 {
     public abstract class AbstractAppsNavigator : AbstractScrollView, IAppsNavigator
     {
+        public Transform content { get { return scrollRect.content; } }
+
         public abstract IPromise Next();
         public abstract IPromise Previous();
         public abstract IPromise SpawnApplication(GameObject app);
