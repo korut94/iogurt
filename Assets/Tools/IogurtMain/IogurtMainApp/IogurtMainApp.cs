b98cec4d67b4d8064345db4163ac6ec726c758bd
Andrea Lisi <andrealisi.12lj@gmail.com>
2018-11-26 16:31:28 +0100

    Removed the IogurtMainApp dummy function

--- Assets/Tools/IogurtMain/IogurtMainApp/IogurtMainApp.cs
+++ Assets/Tools/IogurtMain/IogurtMainApp/IogurtMainApp.cs
@@ -4,13 +4,6 @@ namespace Iogurt.Applications
 {
     public sealed class IogurtMainApp : Application, IConnectInterface, IInstantiateApplicationUI
     {
-        public void CreateAnOtherMenu()
-        {
-            var go = this.InstantiateApplicationUI(this);
-            var app = go.GetComponent<IogurtMainApp>();
-
-            this.ConnectInterfaces(app);
-        }
     }
 }
 
