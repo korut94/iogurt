9faa4206e7112064bc9e94d228d1da0658cea133
Andrea Lisi <andrealisi.12lj@gmail.com>
2018-11-26 16:31:51 +0100

    IogurtMainMenu became an EditorXR menu

--- Assets/Tools/IogurtMain/IogurtMainMenu/IogurtMainMenu.cs
+++ Assets/Tools/IogurtMain/IogurtMainMenu/IogurtMainMenu.cs
@@ -1,14 +1,26 @@
 ﻿using Iogurt.UI;
 using UnityEngine;
+using UnityEditor.Experimental.EditorVR;
+using UnityEditor.Experimental.EditorVR.Menus;
 
 namespace Iogurt
 {
-    public sealed class IogurtMainMenu : MonoBehaviour, IConnectInterface
+    public sealed class IogurtMainMenu : MonoBehaviour, IMenu, IConnectInterface
     {
         [SerializeField]
         AbstractAppsNavigator AppsNavigator;
 
+        public Bounds localBounds { get; private set; }
+
+        public MenuHideFlags menuHideFlags
+        {
+            get { return gameObject.activeSelf ? 0 : MenuHideFlags.Hidden; }
+            set { gameObject.SetActive(value == 0); }
+        }
+
+        public GameObject menuContent { get { return gameObject; } }
         public AbstractAppsNavigator navigator { get { return AppsNavigator; } }
+        public int priority { get { return 1; } }
     }
 }
 
