c0255342f20dfa6319a1540e4ac2c7aea9a1453d
Andrea Lisi <andrealisi.12lj@gmail.com>
2019-01-10 11:32:17 +0100

    Added NumericField script

---
+++ Assets/Scripts/UI/Widget/NumericField.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Iogurt.UI
+{
+    public class NumericField : Selectable, ISubmitHandler, IUsesTooltip
+    {
+        [SerializeField]
+        RadialField RadialFieldPrefab;
+        [SerializeField]
+        TextMeshProUGUI ValueText;
+
+        RadialField m_radialFieldInstance;
+
+        public void OnSubmit(BaseEventData eventData)
+        {
+            var go = this.ShowTooltip(RadialFieldPrefab.gameObject);
+            var radialField = go.GetComponent<RadialField>();
+
+            radialField.OnValueChanged.AddListener(() =>
+            {
+                ValueText.text = radialField.value.ToString();
+            });
+
+            radialField.OnConfirm.AddListener(() =>
+            {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.selectHandler);
+            });
+
+            radialField.Select();
+        }
+    }
+}
+
