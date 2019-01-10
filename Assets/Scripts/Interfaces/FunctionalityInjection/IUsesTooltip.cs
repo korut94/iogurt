1e055f8169bffc4815d55bc2c112a4d9d66d80b1
Andrea Lisi <andrealisi.12lj@gmail.com>
2019-01-10 11:20:43 +0100

    Added IUsesTooltip functionality interface

---
+++ Assets/Scripts/Interfaces/FunctionalityInjection/IUsesTooltip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Iogurt
+{
+    public interface IUsesTooltip
+    {
+    }
+
+    public static class IUsesTooltipMethods
+    {
+        internal static Func<GameObject, GameObject> showTooltip { get; set; }
+        internal static Action<GameObject> closeTooltip { get; set; }
+
+        public static GameObject ShowTooltip(this IUsesTooltip obj, GameObject prefab)
+        {
+            return showTooltip(prefab);
+        }
+
+        public static void CloseTooltip(this IUsesTooltip obj, GameObject tooltip)
+        {
+            closeTooltip(tooltip);
+        }
+    }
+}
+
