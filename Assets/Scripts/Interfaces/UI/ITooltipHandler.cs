e6448ad0b9352296ea6abb0e45183dc20710df4e
Andrea Lisi <andrealisi.12lj@gmail.com>
2019-01-10 11:31:51 +0100

    Added ITooltipHandler interface

---
+++ Assets/Scripts/Interfaces/UI/ITooltipHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Iogurt.UI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ITooltipHandler
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tooltip"></param>
+        void CloseTooltip(GameObject tooltip);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tooltip"></param>
+        /// <returns></returns>
+        GameObject ShowTooltip(GameObject tooltip);
+    }
+}
+
